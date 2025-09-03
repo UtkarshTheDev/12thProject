9cb289b79a496d1ba9c07b1934befe33dda52673
Utkarsh Tiwari <utkarshweb2023@gmail.com>
2025-09-03 13:08:39 +0530

    Add Data Handler

---
+++ data/handler.py
@@ -0,0 +1,17 @@
+
+def extractOutofMarks(df):
+    # Extract 'out of marks' from the first row
+    outofmarks_row = df.iloc[0]
+    df = df.iloc[1:]
+
+    # Create a dictionary for 'out of marks'
+    outofmarks_dict = outofmarks_row.to_dict()
+    return outofmarks_dict
+
+
+def excludeRollNoAndName(df):
+    # Exclude 'Roll No' and 'Name' columns
+    df_numeric = df.select_dtypes(include='number')
+    if 'Roll No' in df_numeric.columns:
+        df_numeric = df_numeric.drop(columns=['Roll No'])
+    return df_numeric
